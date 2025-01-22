b7fe1dea129b8db37d7457a3c1474bd19e4ed40e
Bruno Bastos <brunobbastosf@gmail.com>
2025-01-22 01:31:58 -0300

    feat: adicionando javadoc

--- src/Castings.java
+++ src/Castings.java
@@ -1,35 +1,60 @@
+/**
+ * @author Bruno
+ * @since 2025
+ * @version 1.0
+ */
 public class Castings {
     private int numero1 = 12;
     private short numero2 = 0;
     private Integer numero3 = 2;
-    private char numero4 = 'C';
 
-    public void exibeNumero() {
-        System.out.println(this.numero1);
-    }
-
+    /**
+     *
+     * @return Retorna o casting de int para long.
+     */
     public long castigToLong() {
         long newNumber = numero1;
+        System.out.println(newNumber);
         return newNumber;
     }
 
+    /**
+     *
+     * @return Retorna o casting de int para Integer(Wrapper).
+     */
     public Integer castingToInteger() {
         Integer newNumber = (Integer) numero1;
+        System.out.println(newNumber);
         return newNumber;
     }
 
+    /**
+     *
+     * @return Retorna o casting de Integer para int(primitivo).
+     */
     public int castingToInt() {
         int newNumber = (int) numero3;
+        System.out.println(newNumber);
         return newNumber;
     }
 
-    public char castingToChar() {
-        char newChar = (char) numero2;
-        return  newChar;
-    }
-
+    /**
+     *
+     * @return Retorna o casting de int para Double(Wrapper).
+     */
     public Double castingToDouble() {
         Double newNumber = Double.valueOf(numero1);
+        System.out.println(newNumber);
+        return newNumber;
+    }
+
+    /**
+     *
+     * @return Retorna o casting de int para short.
+     */
+    public int castingIntToShort() {
+        short newNumber = (short) numero1;
+        System.out.println(newNumber);
         return newNumber;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Sistema iniciado.");
+        Castings castings = new Castings();
 
+        castings.castingToInt();
+        castings.castingToDouble();
+        castings.castingToDouble();
+        castings.castingToInteger();
+        castings.castingIntToShort();
+        castings.castigToLong();
+        System.out.println("Sistema finalizado.");
     }
 }
